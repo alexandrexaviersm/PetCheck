2d091c2e20ad55a0ccd80a3afeb406f517c7c0d1
Alexandre Moreira Xavier <alexandrexaviersm@gmail.com>
2022-08-07 23:43:44 -0300

    Install tailwind

--- config/dev.exs
+++ config/dev.exs
@@ -26,7 +26,8 @@ config :pet_check, PetCheckWeb.Endpoint,
   secret_key_base: "xNNZDT+2Gn4n0pgoUZXw283AlvYBboA62UxuKzLJ9bWQCt8zfmuldYTyZAxRzdZY",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
